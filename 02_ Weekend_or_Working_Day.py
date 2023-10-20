797e4b45f253bb51752573a53c97eb692755862d
Diyan Raykov <146946767+DiyanRaykov@users.noreply.github.com>
2023-10-20 21:24:44 +0300

    Add files via upload

---
+++ 02_ Weekend_or_Working_Day.py
@@ -0,0 +1,33 @@
+# day = input()
+#
+# if day == 'Monday':
+#     print('Working day')
+# elif day == 'Tuesday':
+#     print('Working day')
+# elif day == 'Wednesday':
+#     print('Working day')
+# elif day == 'Thursday':
+#     print('Working day')
+# elif day == 'Friday':
+#     print('Working day')
+# elif day == 'Saturday':
+#     print('Weekend')
+# elif day == 'Sunday':
+#     print('Weekend')
+# else:
+#     print('Error')
+# до тук решението е вярно, но дълго
+
+day = input()
+
+if day == 'Monday' or day == 'Tuesday' \
+        or day == 'Wednesday' \
+        or day == 'Thursday' \
+        or day == 'Friday':
+    print('Working day')
+
+elif day == 'Saturday' \
+        or day == 'Sunday':
+    print('Weekend')
+else:
+    print('Error')
