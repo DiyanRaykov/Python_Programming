0ae87198937b52d1fa2f694e17e025a2ab99258b
Diyan Raykov <146946767+DiyanRaykov@users.noreply.github.com>
2023-10-26 19:30:59 +0300

    Add files via upload

---
+++ 10_Invalid_Number.py
@@ -0,0 +1,12 @@
+number = int(input())
+
+# if not(100 <= number <= 200 or number == 0):
+#     print('invalid')
+
+# И горното и долното решение са правилни.
+
+if 100 <= number <= 200 or number == 0:
+    pass
+else:
+    print('invalid')
+
