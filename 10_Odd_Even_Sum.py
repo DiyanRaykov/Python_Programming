bb24475d5b5193ca11ac7fcfd8593bfacb618995
Diyan Raykov <146946767+DiyanRaykov@users.noreply.github.com>
2023-11-10 18:12:44 +0200

    Add files via upload

---
+++ 10_Odd_Even_Sum.py
@@ -0,0 +1,18 @@
+count = int(input())
+odd_sum = 0
+even_sum = 0
+
+for i in range(0, count):
+    number = int(input())
+    if i % 2 == 0:
+        even_sum += number
+    else:
+        odd_sum += number
+
+diff = abs(odd_sum - even_sum)
+if odd_sum == even_sum:
+    print('Yes')
+    print(f'Sum = {odd_sum}')
+else:
+    print('No')
+    print(f'Diff = {diff}')
